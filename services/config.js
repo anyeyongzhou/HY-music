64f074906468e4d953195c5a03c1c725addafec8
anyeyongzhou <1354785752@qq.com>
2023-08-20 09:20:05 +0800

    按需引入

--- services/config.js
+++ services/config.js
@@ -1,2 +1,4 @@
-export const baseUrl="http://123.207.32.32:9002"
+//http://codercba.com:9002/
+//http://123.207.32.32:9002
+export const baseUrl="https://codercba.com:9002"
 // export const baseUrl="https://coderwhy-music.vercel.app/"
